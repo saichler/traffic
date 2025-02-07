0fb46b23ddafa5b9995ab69a451e51666dd997ec
Sharon Aicler <saichler@cisco.com>
2025-02-07 09:16:31 -0800

    Start docker build

---
+++ docker/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+cd ../go/generator
+GOARCH=amd64 GOOS=linux go build -o ../../docker/generator
+cd ../../docker
+
+docker build --platform=linux/amd64 -t saichler/traffic-generator:latest .
---
+++ docker/start-listener.sh
@@ -0,0 +1,1 @@
+docker run -e CMD=Start -e Udp_port='15000' -e Tcp_port='16000' saichler/traffic-generator:latest
